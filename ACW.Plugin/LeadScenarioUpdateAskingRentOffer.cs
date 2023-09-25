894a1d779706b460369ee341ac9dd18a3ca9e12c
Will Chen <willc@ACW365.onmicrosoft.com>
2023-09-25 10:21:59 +0800

    update plugin

---
+++ ACW.Plugin/LeadScenarioUpdateAskingRentOffer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace ACW.Plugin
+{
+    public class LeadScenarioUpdateAskingRentOffer : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Lead Scenario")]
+        [ReferenceTarget("clf_leadscenario")]
+        public InArgument<EntityReference> _leadSc { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+
+            try
+            {
+                EntityReference leadSc = this._leadSc.Get(executionContext);
+                tracer.Trace("Lead Scenario: {0}", leadSc.Id.ToString());
+
+                QueryExpression qe = new QueryExpression("clf_offer");
+                qe.Criteria.AddCondition("lms_leadscenario", ConditionOperator.Equal, leadSc.Id);
+                qe.Criteria.AddCondition("clf_offertype", ConditionOperator.Equal, 100000000);
+                qe.ColumnSet.AddColumn("statuscode");
+
+                EntityCollection ec = service.RetrieveMultiple(qe);
+
+                if (ec.Entities.Count > 0)
+                {
+                    foreach (var offer in ec.Entities)
+                    { 
+                        Entity updateEn = new Entity(offer.LogicalName, offer.Id);
+                        updateEn["statuscode"] = new OptionSetValue(176660005);
+                        service.Update(updateEn);
+
+                        tracer.Trace("Updated offer: {0}", offer.Id.ToString());
+                    }
+                }
+
+                else
+                    tracer.Trace("No offer");
+            }
+            catch (Exception ex)
+            {
+                string msg = $"{this.GetType().Name} of Error Message: \r\n \t {ex.Message}";
+                tracer.Trace(msg);
+                throw new InvalidPluginExecutionException(msg);
+            }
+        }
+    }
+}
+
---
+++ ACW.Plugin/LeaseIdRenewalCompareHoldoverEnd.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACW.Plugin
+{
+    public class LeaseIdRenewalCompareHoldoverEnd : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Lease ID")]
+        public InArgument<string> _leaseId { get; set; }
+
+        [RequiredArgument]
+        [Input("Holdover End")]
+        public InArgument<DateTime> _holdoverEnd { get; set; }
+
+        [Output("Output")]
+        public OutArgument<bool> _output { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
+            ITracingService tracer = executionContext.GetExtension<ITracingService>();
+
+            try
+            {
+                string leaseId = this._leaseId.Get(executionContext);
+                DateTime holdoverEnd = this._holdoverEnd.Get(executionContext);
+                
+
+                if (leaseId == "")
+                    throw new InvalidPluginExecutionException("Lease ID is empty.");
+
+                if (holdoverEnd.Year == 1)
+                    throw new InvalidPluginExecutionException("Holdover End Datetime is empty");
+
+                tracer.Trace("Leasd Id: {0}", leaseId);
+                tracer.Trace("HoldOver End: {0}", holdoverEnd.ToString());
+
+                EntityCollection ec = GetLease(service, leaseId, holdoverEnd);
+                tracer.Trace("Count: {0}", ec.Entities.Count.ToString());
+
+                if (ec.Entities.Count >= 1)
+                    this._output.Set(executionContext, true);
+                else
+                    this._output.Set(executionContext, false);
+            }
+            catch (Exception ex)
+            {
+                string msg = $"{this.GetType().Name} of Error Message: \r\n \t {ex.Message}";
+                tracer.Trace(msg);
+                throw new InvalidPluginExecutionException(msg);
+            }
+        }
+
+        private EntityCollection GetLease(IOrganizationService service, string leaseId, DateTime holdoverEnd)
+        {
+            QueryExpression qe = new QueryExpression("lms_lease");
+            qe.Criteria.AddCondition("lms_leasenumber", ConditionOperator.Equal, leaseId);
+            qe.Criteria.AddCondition("lms_leasetype", ConditionOperator.Equal, 1);
+            qe.Criteria.AddCondition("lms_leasestart", ConditionOperator.OnOrBefore, holdoverEnd);
+            qe.Criteria.AddCondition("lms_leaseend", ConditionOperator.OnOrAfter, holdoverEnd);
+
+            EntityCollection ec = service.RetrieveMultiple(qe);
+
+            return ec;
+        }
+    }
+}
+
